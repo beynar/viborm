1378d27ed88c1df45b8a546ef4221df3cd0c75fa
beynar <arnaud@derbey.dev>
2025-05-29 11:39:18 +0200

    fix field filter type

--- src/types/client/query/filters.ts
+++ src/types/client/query/filters.ts
@@ -311,14 +311,8 @@ export type FieldFilter<F extends BaseField<any>> = F extends DateTimeField<any>
   ? IsFieldNullable<F> extends true
     ? JsonNullableFilter | any | null
     : JsonFilter | any
-  : F extends EnumField<any>
-  ? F extends { "~enumValues": infer E }
-    ? IsFieldNullable<F> extends true
-      ? E extends (string | number)[]
-        ? EnumNullableFilter<E[number]> | E[number] | null
-        : never
-      : E extends (string | number)[]
-      ? EnumFilter<E[number]> | E[number]
-      : never
-    : never
+  : F extends EnumField<infer E>
+  ? IsFieldNullable<F> extends true
+    ? EnumNullableFilter<E[number]> | E[number] | null
+    : EnumFilter<E[number]> | E[number]
   : never;
